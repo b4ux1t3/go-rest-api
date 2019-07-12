90419f77ebbe8f2455fd67bb08cbdd0d83e6a70d
Chris Pilcher <christopher.a.pilcher@gmail.com>
2019-07-12 14:46:57 -0400

    Added proper HTTP error for invalid request path

--- rest.go
+++ rest.go
@@ -23,10 +23,10 @@ var templates = template.Must(template.ParseFiles("result.html", "index.html"))
 func collatzHandler(w http.ResponseWriter, r *http.Request) {
 	argumentIndex := len("/collatz/")
 	argument, err := strconv.ParseUint(r.URL.Path[argumentIndex:], 10, 64)
-
+	// This executes if we do not receive aninteger from the user after "/collatz/" in the URL path.
 	if err != nil {
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		//http.Error(w, err.Error(), http.StatusInternalServerError)
+		//http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		http.Error(w, "Unable to parse argument. URL path should be: /collatz/<POSITIVE BASE 10 INTEGER>", http.StatusInternalServerError)
 		return
 	}
 
